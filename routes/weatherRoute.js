5538c3c9ebd34eb12c9acd0883f5a6e4460457f6
vishal sharma <vishallsharma07@gmail.com>
2021-10-24 10:22:35 +0530

    update

--- routes/weatherRoute.js
+++ routes/weatherRoute.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
+var needle = require('needle');
+const url = require('url')
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
 
-router.get('/',(req, res) =>{
-    res.send({message : "Working"})
+router.get('/', async(req, res) =>{
+    try {
+        console.log(url.parse(req.url, true).query)
+        const params = new URLSearchParams({
+            APPID: API_KEY_VALUE,
+            ...url.parse(req.url, true).query
+        })
+        console.log(`${API_BASE_URL}?${params}`)
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+        const data = apiRes.body
+        res.status(200).json(data)
+    }catch(error){
+        res.status(500).json({message : error.message})
+    }
+    
 })
 
 module.exports = router
